a559801971750b473d725b5870099bebc2f9752c
jie chen <jiechen@jiedeMacBook-Pro.local>
2020-01-16 16:39:04 +0800

    webpack模式

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './src/app.js',
-        vendors: './src/vendors.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'https://www.37.com/',
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.txt/,
-                use: 'raw-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+    mode: 'production',
+    // entry: {
+    //     app: './src/app.js',
+    //     vendors: './src/vendors.js'
+    // },
+    // output: {
+    //     path: path.resolve(__dirname, 'dist'),
+    //     filename: '[name].js'
+    // },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.txt/,
+    //             use: 'raw-loader'
+    //         }
+    //     ]
+    // },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: './src/index.html'
+    //     })
+    // ]
 };
